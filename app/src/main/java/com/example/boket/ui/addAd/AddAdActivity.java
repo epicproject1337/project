6439b028d93e6319a2423c90c24a3c1e825d1715
Jyborn <alexander.jyborn@gmail.com>
2020-09-08 18:24:21 +0200

    activity_add_ad xml done

--- app/src/main/java/com/example/boket/ui/addAd/AddAdActivity.java
+++ app/src/main/java/com/example/boket/ui/addAd/AddAdActivity.java
@@ -3,6 +3,9 @@ package com.example.boket.ui.addAd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.boket.R;
 
@@ -12,5 +15,11 @@ public class AddAdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ad);
+
+        final EditText conditionEditText = findViewById(R.id.conditionInput);
+        final EditText priceEditText = findViewById(R.id.priceInput);
+        final EditText cityEditText = findViewById(R.id.cityInput);
+        final Button publishButton = findViewById(R.id.publishButton);
+
     }
 }
