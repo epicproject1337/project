97e4e122b1308fdbdc7da5315a86f6ccb4e37a1c
Jyborn <alexander.jyborn@gmail.com>
2020-09-08 17:55:24 +0200

    activity_add_ad xml done

---
+++ app/src/main/java/com/example/boket/ui/addAd/AddAdActivity.java
@@ -0,0 +1,16 @@
+package com.example.boket.ui.addAd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.boket.R;
+
+public class AddAdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ad);
+    }
+}
