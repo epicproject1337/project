a610250f1a3b99837e61cde747dfe0f32274277a
albin <albin.landgren@trinax.se>
2020-09-10 15:27:23 +0200

    Added horizontal image scroll to profile

---
+++ app/src/main/java/com/example/boket/ui/profile/RecyclerViewHolder.java
@@ -0,0 +1,32 @@
+package com.example.boket.ui.profile;
+
+import android.media.Image;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.boket.R;
+
+import java.util.List;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    private ImageView view;
+    public RecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+        view = itemView.findViewById(R.id.bookimage);
+    }
+
+    public ImageView getView(){
+        return view;
+    }
+}
