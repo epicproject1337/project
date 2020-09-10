c125e3d96c6f20f2a4e98731088509af6b2f175c
Oscar Bennet <32869912+obennet@users.noreply.github.com>
2020-09-10 13:58:43 +0200

    Created search page

---
+++ app/src/main/java/com/example/boket/ui/search/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.boket.ui.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
