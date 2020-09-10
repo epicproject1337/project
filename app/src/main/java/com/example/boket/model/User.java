118eba99e5c2ef13056eb7c8352bec761e25b7d9
Pajam <pajamkh@gmail.com>
2020-09-10 16:11:38 +0200

    Started making user model class

---
+++ app/src/main/java/com/example/boket/model/User.java
@@ -0,0 +1,47 @@
+package com.example.boket.model;
+
+import com.example.boket.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class User{
+
+    private int uid;
+    private String email;
+    private String name;
+    private String location;
+
+    private FirebaseAuth mAuth;
+    private static final String TAG = User.class.getName();
+
+    public User() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void signup(String name, String email, String password, String location) {
+        //TODO
+    }
+
+    public void login(String email, String password){
+        //TODO
+    }
+
+    public void signout(){
+        //TODO
+    }
+}
