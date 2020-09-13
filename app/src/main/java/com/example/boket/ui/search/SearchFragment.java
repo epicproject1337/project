5a4493fd83022579e0b70799d16df20d385aa3c1
Oscar Bennet <32869912+obennet@users.noreply.github.com>
2020-09-13 21:08:14 +0200

    Created fetchBooks method on searchPage (incomplete)

--- app/src/main/java/com/example/boket/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/boket/ui/search/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.example.boket.ui.search;
 
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -17,13 +18,18 @@ import android.widget.ImageView;
 import com.example.boket.R;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
 
-    private SearchViewModel mViewModel;
+    private SearchViewModel searchViewModel;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
     }
+
+    private TextInputLayout searchBar;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -31,20 +37,19 @@ public class SearchFragment extends Fragment {
 
 
         //onClickListener
-        BooksellersFragment  booksellersFragment = new BooksellersFragment();
+        BooksellersFragment booksellersFragment = new BooksellersFragment();
         Bundle bundle = new Bundle();
-        bundle.putString("BookNumber","ISBN-nummer"); //Skriv valda bokens ISBN-nummer
+        bundle.putString("BookNumber", "ISBN-nummer"); //Skriv valda bokens ISBN-nummer
         booksellersFragment.setArguments(bundle);
         FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment,booksellersFragment).commit();
+        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, booksellersFragment).commit();
         //getFragmentManager().beginTransaction().replace(BooksellersFragment,this)
 
-        View v = inflater.inflate(R.layout.fragment_search,container,false);
-        TextInputLayout searchBar = v.findViewById(R.id.searchBar);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        searchBar = v.findViewById(R.id.searchBar);
 
 
         return inflater.inflate(R.layout.fragment_search, container, false);
-
 
 
     }
@@ -52,8 +57,19 @@ public class SearchFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    public String getInput() {
+        return searchBar.getEditText().toString();
+    }
+
+    private ArrayList<String> fetchBooks() {  //Change type of list to Book object
+        String input = getInput();
+        ArrayList books = searchViewModel.fetchBooks(input);
+
+        return books;
     }
 
 }
--- app/src/main/java/com/example/boket/ui/search/SearchViewModel.java
+++ app/src/main/java/com/example/boket/ui/search/SearchViewModel.java
@@ -2,6 +2,24 @@ package com.example.boket.ui.search;
 
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+
 public class SearchViewModel extends ViewModel {
+
+    private ArrayList<String> books;
+
+    public ArrayList<String> fetchBooks(String input) { //Change type of list to Book object
+        books = null; //fetch from database
+
+        return books;
+    }
+
+    public ArrayList<String> getBooks(){
+        return books;
+    }
+
     // TODO: Implement the ViewModel
+
+
+
 }
