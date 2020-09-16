03f0f9698e26cbd1f4c09e77228f5f10b73aff1d
albin <albin.landgren@trinax.se>
2020-09-16 17:43:02 +0200

    Work on restructuring navbar

---
+++ app/src/main/java/com/example/boket/ui/addAd/AddAdFragment.java
@@ -0,0 +1,26 @@
+package com.example.boket.ui.addAd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.boket.R;
+
+public class AddAdFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        Intent intent = new Intent(getActivity(), AddAdActivity.class);
+        startActivity(intent);
+        return root;
+    }
+}
