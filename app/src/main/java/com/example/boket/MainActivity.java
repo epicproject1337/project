469278e0ac8a6a4975f9065e050540812b19d2b4
albin <albin.landgren@trinax.se>
2020-09-16 19:28:01 +0200

    Added floating action button for camera

--- app/src/main/java/com/example/boket/MainActivity.java
+++ app/src/main/java/com/example/boket/MainActivity.java
@@ -9,12 +9,16 @@ import android.widget.Toast;
 import com.example.boket.ui.addAd.AddAdActivity;
 import com.example.boket.ui.camera.BarcodeScannerActivity;
 import com.example.boket.ui.login.LoginActivity;
+import com.example.boket.ui.profile.ProfileFragment;
+import com.example.boket.ui.search.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -40,19 +44,17 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(this, "Already logged in", Toast.LENGTH_LONG).show();
         }
 
-
-
-
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_search, R.id.navigation_add_ad, R.id.navigation_profile)
+                R.id.navigation_search, R.id.navigation_profile)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
+
 
 
     }
