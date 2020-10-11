9d0c68cee0817e9db4fe763720e0af9afd8c9f78
Pajam <pajamkh@gmail.com>
2020-10-11 13:02:04 +0200

    Added empty notifier class

---
+++ app/src/main/java/com/example/boket/controller/Notifier.java
@@ -0,0 +1,14 @@
+package com.example.boket.controller;
+
+public final class Notifier {
+
+
+    private Notifier(){
+
+    }
+
+    //TODO Add logic
+    private static void notifyUsersAboutNewAd(String isbn){
+
+    }
+}
