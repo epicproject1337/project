43f6f74f6fa296d55ff4148ce519f0e67d845d49
Pajam <pajamkh@gmail.com>
2020-10-23 11:55:49 +0200

    Cleaned up signup activity

--- app/src/main/java/com/example/boket/ui/login/SignupActivity.java
+++ app/src/main/java/com/example/boket/ui/login/SignupActivity.java
@@ -1,7 +1,6 @@
 package com.example.boket.ui.login;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -16,32 +15,23 @@ import android.widget.TextView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.boket.MainActivity;
 import com.example.boket.R;
 import com.example.boket.model.user.LocalUser;
-import com.example.boket.model.user.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
-import org.w3c.dom.Text;
 
 /**
  * @author Pajam Khoshnam
- * <p>
  * The Activity for signing up a user
  * @since 2020-09-07
  */
 public class SignupActivity extends AppCompatActivity {
 
     private static final String TAG = SignupActivity.class.getName();
-    private Drawable originTVcolor;
+    private Drawable originTextColor;
+    private ProgressBar loadingProgressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,20 +46,19 @@ public class SignupActivity extends AppCompatActivity {
         final EditText passwordConfirmEditText = findViewById(R.id.passwordConfirm);
         final Button signupButton = findViewById(R.id.signup);
         final Button goTologin = findViewById(R.id.goToLogin);
-        originTVcolor = nameEditText.getBackground();
+        originTextColor = nameEditText.getBackground();
 
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        loadingProgressBar = findViewById(R.id.loading);
 
-        setTVwriteListener(nameEditText);
-        setTVwriteListener(emailEditText);
-        setTVwriteListener(emailConfirmEditText);
-        setTVwriteListener(passwordEditText);
-        setTVwriteListener(passwordConfirmEditText);
+        setTextColorWriteListener(nameEditText);
+        setTextColorWriteListener(emailEditText);
+        setTextColorWriteListener(emailConfirmEditText);
+        setTextColorWriteListener(passwordEditText);
+        setTextColorWriteListener(passwordConfirmEditText);
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //loadingProgressBar.setVisibility(View.VISIBLE);
 
                 if (!checkInputs()) {
                     Log.e("SIGNUP", "SIGNUP NOT VALID");
@@ -82,6 +71,7 @@ public class SignupActivity extends AppCompatActivity {
                 String password = emailConfirmEditText.getText().toString();
                 String passwordConfirm = passwordConfirmEditText.getText().toString();
 
+                loadingProgressBar.setVisibility(View.VISIBLE);
                 signup(name, email, emailConfirm, password, passwordConfirm);
 
             }
@@ -138,7 +128,7 @@ public class SignupActivity extends AppCompatActivity {
         });
     }
 
-    private void setTVwriteListener(TextView tv) {
+    private void setTextColorWriteListener(TextView tv) {
         tv.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -147,7 +137,7 @@ public class SignupActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                tv.setBackground(originTVcolor);
+                tv.setBackground(originTextColor);
             }
 
             @Override
@@ -158,22 +148,18 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     private void signup(final String name, String email, String emailConfirm, String password, String passwordConfirm) {
-        //TODO: Validate email and password
-
-        System.out.println(email);
-        System.out.println(emailConfirm);
 
         LocalUser.signup(name, email, emailConfirm, password, passwordConfirm, "Göteborg", new LocalUser.SignupCallback() {
             @Override
             public void onSignupComplete(LocalUser user) {
                 Log.d(TAG, "createUserWithEmail:success");
                 LocalUser.getCurrentUser();
-                //Update the UI
                 updateUiWithUser(user.getName());
             }
 
             @Override
             public void onSignupFailed(String message) {
+                loadingProgressBar.setVisibility(View.GONE);
                 showLoginFailed("Signup failed! Try again.. " + message);
             }
         });
@@ -182,7 +168,6 @@ public class SignupActivity extends AppCompatActivity {
     //TODO: LoginActivity have same method. Merge somehow.
     private void updateUiWithUser(String displayName) {
         String welcome = "Welcome! " + displayName;
-        // TODO : initiate successful logged in experience
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -196,25 +181,7 @@ public class SignupActivity extends AppCompatActivity {
 
     }
 
-    //TODO: LoginActivity have same method. Merge somehow.
     private void showLoginFailed(String errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
-    }
-
-    //TODO : Add error handling
-    private void updateUsersName(String name, FirebaseUser user) {
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(name)
-                .build();
-
-        user.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "User profile updated.");
-                        }
-                    }
-                });
     }
 }
