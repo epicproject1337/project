efa7ce06b6c041fece6d0d93f7aa6e8a011d577c
Oscar Bennet <32869912+obennet@users.noreply.github.com>
2020-10-23 16:09:24 +0200

    New test for searchFragment

---
+++ app/src/androidTest/java/com/example/boket/SearchFragmentTest.java
@@ -0,0 +1,190 @@
+package com.example.boket;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.boket.model.Book;
+import com.example.boket.ui.search.SearchFragment;
+import com.example.boket.ui.search.SearchViewModel;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SearchFragmentTest {
+
+    private SearchFragment searchFragment;
+    private SearchViewModel searchViewModel;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    private void startSearchFragment() {
+        MainActivity activity = (MainActivity) mActivityTestRule.getActivity();
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+        searchFragment = new SearchFragment();
+        transaction.add(searchFragment, "searchFragment");
+        transaction.commit();
+    }
+
+    @Before
+    public void setup() {
+        onView(isRoot()).perform(doTaskInUIThread(new Runnable() {
+            @Override
+            public void run() {
+                startSearchFragment();
+
+            }
+        }));
+    }
+
+    public static ViewAction doTaskInUIThread(final Runnable r) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                r.run();
+            }
+        };
+    }
+
+    @Test
+    public void testViewModelIsWorking() throws Exception {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+
+                //Task that need to be done in UI Thread (below I am adding a fragment)
+                searchViewModel = ViewModelProviders.of(searchFragment).get(SearchViewModel.class);
+
+                ArrayList<Book> books = new ArrayList<>();
+                Book book = new Book("9789144090504", "Algebra och diskret matematik", "Johan Jonasson, Stefan Lemurell", "2", "2013", "https://s1.adlibris.com/images/3075059/algebra-och-diskret-matematik.jpg");
+                books.add(book);
+                searchViewModel.setBooks(books);
+
+                assertEquals(1, searchViewModel.getBooks().size());
+            }
+        };
+        onView(isRoot()).perform(doTaskInUIThread(r));
+    }
+
+    @Test
+    public void recyclerViewContainsBookItem(){
+        ViewInteraction appCompatImageView = onView(
+                allOf(withClassName(is("androidx.appcompat.widget.AppCompatImageView")), withContentDescription("Search"),
+                        childAtPosition(
+                                allOf(withClassName(is("android.widget.LinearLayout")),
+                                        childAtPosition(
+                                                withId(R.id.searchView),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageView.perform(click());
+
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")),
+                        childAtPosition(
+                                allOf(withClassName(is("android.widget.LinearLayout")),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete.perform(replaceText("Linjär algebra"), closeSoftKeyboard());
+
+
+
+        ViewInteraction searchAutoComplete2 = onView(
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("Linjär algebra"),
+                        childAtPosition(
+                                allOf(withClassName(is("android.widget.LinearLayout")),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete2.perform(pressImeActionButton());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction viewGroup = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.recyclerView),
+                                0),
+                        0),
+                        isDisplayed()));
+        viewGroup.check(matches(isDisplayed()));
+    }
+
+
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
